8771e67fdfc8a32d47ae0b58125d5aaa2fcfcdfe
dzpu <42391788+dzpu@users.noreply.github.com>
2018-10-23 19:39:16 -0700

    Fixing the Code

---
+++ notes/Basic Python Note.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+# Apparently I'm going too slow, so I will speed up
+# This is a comment
+# This has no effect on the code
+# This is used for a variety of things, such as
+# 1. Making personal notes about my code
+# 2. Commenting out code that does not work
+
+print("Notice what is happening here.")
+print()  # This creates a new line
+print()  # Do you notice the underline here?
+# Hover over top it to see what is wrong

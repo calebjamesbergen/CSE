3317626d7b39e2734582064a68c8c712163e0670
dzpu <42391788+dzpu@users.noreply.github.com>
2018-10-29 17:13:42 -0700

    Making Magic 8 Ball

--- notes/Magic 8 Ball.py
+++ notes/Magic 8 Ball.py
@@ -1,4 +1,17 @@
 while 0 == 0:
-    question = input("Ask me a question ")
-    answer = "Yes" or "No" or "Maybe" or "As likely as ice cream raining from the sky"
+    answer = ""
+    import random
+    for x in range(1):
+        x = random.randint(1, 5)
+    if x == 1:
+        answer = "Yes"
+    if x == 2:
+        answer = "No"
+    if x == 3:
+        answer = "Maybe"
+    if x == 4:
+        answer = "As likely as ice cream raining from the sky"
+    if x == 5:
+        answer = "As likely as Donald Trump being reelected for president"
+    question = input("Ask me a yes or no question ")
     print("%s" % answer)

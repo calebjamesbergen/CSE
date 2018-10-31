ecf746357e3d78cb8dba6933d1c5697aea731161
dzpu <42391788+dzpu@users.noreply.github.com>
2018-10-31 14:36:26 -0700

    Class Work 10-31-18

--- notes/Basic Python Note.py
+++ notes/Basic Python Note.py
@@ -115,3 +115,54 @@ def distance(x1, y1, x2, y2):
 
 distance(0, 0, 3, 4)
 distance(0, 0, 5, 12)
+
+# For loops
+# In this case "i" is a variable
+for i in (1, 2, 3):
+    say_it()
+
+for i in range(5):    # Range(5) gives the numbers 0-4
+    f(i)
+
+for i in range(5):
+    print(i ** 2)
+# Don't do too many for loops in a for loop
+
+# While Loop
+a = 0
+while a < 10:
+    print(a)
+    a += 1  # This is the same as a = a + 1
+
+
+"""
+Hints for loops:
+For loops - Use when you know EXACTLY how many iterations
+While loops - Use when you DON'T know how many iterations
+"""
+
+# Control Statement (If statements)
+sunny = False
+if sunny:
+    print("Go outside")
+
+
+def grade_calc(percentage):
+    if percentage >= 90:  # >= Equivalent of greater then or equal to
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+your_grade = grade_calc(82)
+print(your_grade)
+
+# Random numbers
+import random    # This should be on line  1
+print(random.randint(0, 100))

71c4b92e6b9be4de856d7f9c9324910f9fe60020
dzpu <42391788+dzpu@users.noreply.github.com>
2018-11-05 14:35:26 -0800

    Class Work 11-5-18

--- notes/Hangman.py
+++ notes/Hangman.py
@@ -5,24 +5,24 @@ second_letter = "r"
 third_letter = "i"
 fourth_letter = "c"
 fifth_letter = "k"
+letter_guessed1 = ""
 letter_guessed2 = ""
+letter_guessed3 = ""
+letter_guessed4 = ""
+letter_guessed5 = ""
 
 # Random word generation
-while guesses > 0 or not letter_guessed2 == word:
+while guesses >= 0:
     letter_guessed = input("Guess a letter ")
     guesses = guesses - 1
     if letter_guessed == first_letter:
-        letter_guessed2 = letter_guessed2 + letter_guessed
-        print(letter_guessed2)
+        letter_guessed1 = letter_guessed
     elif letter_guessed == second_letter:
-        letter_guessed2 = letter_guessed2 + letter_guessed
-        print(letter_guessed2)
+        letter_guessed2 = letter_guessed
     elif letter_guessed == third_letter:
-        letter_guessed2 = letter_guessed2 + letter_guessed
-        print(letter_guessed2)
+        letter_guessed3 = letter_guessed
     elif letter_guessed == fourth_letter:
-        letter_guessed2 = letter_guessed2 + letter_guessed
-        print(letter_guessed2)
+        letter_guessed4 = letter_guessed
     elif letter_guessed == fifth_letter:
-        letter_guessed2 = letter_guessed2 + letter_guessed
-        print(letter_guessed2)
+        letter_guessed5 = letter_guessed
+    print("%s%s%s%s%s" % (letter_guessed1, letter_guessed2, letter_guessed3, letter_guessed4, letter_guessed5))

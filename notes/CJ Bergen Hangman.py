9ed3cead4f7b64278309b42da9f2da20567c0a77
dzpu <42391788+dzpu@users.noreply.github.com>
2018-11-14 14:36:55 -0800

    Class Work 11-14-18

--- notes/CJ Bergen Hangman.py
+++ notes/CJ Bergen Hangman.py
@@ -5,10 +5,16 @@ letter_guessed2 = ""
 letter_guessed3 = ""
 letter_guessed4 = ""
 letter_guessed5 = ""
+letter_guessed6 = ""
+letter_guessed7 = ""
+letter_guessed8 = ""
+letter_guessed9 = ""
+letter_guessed10 = ""
 word_guessed = ""
 random_word_list = ["pricy     ", "roast     ", "tenor     ", "tense     ", "loser     ", "train     ",
-                    "tears     ", "jerky     ", "treat     ", "siren     ", "stair     ", "arise     ", "tires     ", "arson     ", "jacky     ", "brick     ", "chair     "
-                    "tread     ", "trail     ", "pouch", "jumbucks     ", "juxtaposed"]
+                    "tears     ", "jerky     ", "treat     ", "siren     ", "stair     ", "arise     ",
+                    "tires     ", "arson     ", "jacky     ", "brick     ", "chair     ", "tread     ", "trail     ",
+                    "pouch     ", "abdomens  ", "juxtaposed"]
 truly_random_word = random.choice(random_word_list)
 first_letter = truly_random_word[0]
 second_letter = truly_random_word[1]
@@ -20,7 +26,18 @@ seventh_letter = truly_random_word[6]
 eighth_letter = truly_random_word[7]
 ninth_letter = truly_random_word[8]
 tenth_letter = truly_random_word[9]
-# Random word generation
+if not truly_random_word[5] == " ":
+    sixth_letter = truly_random_word[5]
+if not truly_random_word[6] == " ":
+    seventh_letter = truly_random_word[6]
+if not truly_random_word[7] == " ":
+    eighth_letter = truly_random_word[7]
+if not truly_random_word[8] == " ":
+    ninth_letter = truly_random_word[8]
+if not truly_random_word[9] == " ":
+    tenth_letter = truly_random_word[9]
+
+
 while guesses > 0 and not word_guessed == truly_random_word:
     print("You have %s guesses left" % guesses)
     letter_guessed = input("Guess a lowercase letter ")
@@ -35,10 +52,29 @@ while guesses > 0 and not word_guessed == truly_random_word:
         letter_guessed4 = letter_guessed
     elif letter_guessed == fifth_letter:
         letter_guessed5 = letter_guessed
-    word_guessed = letter_guessed1 + letter_guessed2 + letter_guessed3 + letter_guessed4 + letter_guessed5
+    elif letter_guessed == sixth_letter:
+        letter_guessed6 = letter_guessed
+    elif letter_guessed == seventh_letter:
+        letter_guessed7 = letter_guessed
+    elif letter_guessed == eighth_letter:
+        letter_guessed8 = letter_guessed
+    elif letter_guessed == ninth_letter:
+        letter_guessed9 = letter_guessed
+    elif letter_guessed == tenth_letter:
+        letter_guessed10 = letter_guessed
     print(word_guessed)
 
 if word_guessed == truly_random_word:
     print("You win!")
 else:
     print("You lose. Ha!")
+"""
+    elif truly_random_word[5] == " ":
+        word_guessed = letter_guessed1 + letter_guessed2 + letter_guessed3 + letter_guessed4 + letter_guessed5
+    elif truly_random_word == "abdomens  ":
+        word_guessed = (letter_guessed1 + letter_guessed2 + letter_guessed3 + letter_guessed4 + letter_guessed5 +
+                    letter_guessed6 + letter_guessed7 + letter_guessed8)
+    elif truly_random_word == "juxtaposed":
+        word_guessed = (letter_guessed1 + letter_guessed2 + letter_guessed3 + letter_guessed4 + letter_guessed5 +
+                    letter_guessed6 + letter_guessed7 + letter_guessed8 + letter_guessed9 + letter_guessed10)
+"""

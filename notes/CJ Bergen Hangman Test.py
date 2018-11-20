ff2d400439f3efce86b5236a5ebb15aa3ce70634
dzpu <42391788+dzpu@users.noreply.github.com>
2018-11-19 18:36:16 -0800

    ajk;fjad;sj;fd 11-19-18

--- notes/CJ Bergen Hangman Test.py
+++ notes/CJ Bergen Hangman Test.py
@@ -1,6 +1,5 @@
 import random
 guesses = 10
-word_guessed = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 letter_guessed1 = ""
 letter_guessed2 = ""
 letter_guessed3 = ""
@@ -11,11 +10,14 @@ letter_guessed7 = ""
 letter_guessed8 = ""
 letter_guessed9 = ""
 letter_guessed10 = ""
+word_guessed = ""
 random_word_list = ["pricy", "roast", "tenor", "tense", "loser", "train", "tears", "jerky", "abhor", "siren", "stair",
                     "arise", "tires", "arson", "jacky", "brick", "chair", "tread", "trail", "pouch", "abdomens",
                     "juxtaposed"]
 truly_random_word = random.choice(random_word_list)
-letters = list(truly_random_word)
+letters = truly_random_word[0] + truly_random_word[1] + truly_random_word[2] + truly_random_word[3] + \
+          truly_random_word[4] + truly_random_word[5] + truly_random_word[6] + truly_random_word[7] + \
+          truly_random_word[8] + truly_random_word[9]
 if not truly_random_word == "abdomens  " or "juxtaposed":
     letter_guessed6 = " "
     letter_guessed7 = " "
@@ -30,29 +32,29 @@ while guesses > 0 and not word_guessed == truly_random_word:
     letter_guessed = input("Guess a lowercase letter ")
     guesses -= 1
     if letter_guessed == letters[0]:
-        word_guessed.index[0] = letter_guessed
+        letter_guessed1 = letter_guessed
     elif letter_guessed == letters[1]:
-        word_guessed[1] = letter_guessed
+        letter_guessed2 = letter_guessed
     elif letter_guessed == letters[2]:
-        word_guessed[2] = letter_guessed
+        letter_guessed3 = letter_guessed
     elif letter_guessed == letters[3]:
-        word_guessed[3] = letter_guessed
+        letter_guessed4 = letter_guessed
     elif letter_guessed == letters[4]:
-        word_guessed[4] = letter_guessed
+        letter_guessed5 = letter_guessed
     elif letter_guessed == letters[5]:
-        word_guessed[5] = letter_guessed
+        letter_guessed6 = letter_guessed
     elif letter_guessed == letters[6]:
-        word_guessed[6] = letter_guessed
+        letter_guessed7 = letter_guessed
     elif letter_guessed == letters[7]:
-        word_guessed[7] = letter_guessed
+        letter_guessed8 = letter_guessed
     elif letter_guessed == letters[8]:
-        word_guessed[8] = letter_guessed
+        letter_guessed9 = letter_guessed
     elif letter_guessed == letters[9]:
-        word_guessed[9] = letter_guessed
+        letter_guessed10 = letter_guessed
+    word_guessed = list(letter_guessed1 + letter_guessed2 + letter_guessed3 + letter_guessed4 + letter_guessed5 +
+                            letter_guessed6 + letter_guessed7 + letter_guessed8 + letter_guessed9 + letter_guessed10)
     print(word_guessed)
 if word_guessed == truly_random_word:
     print("You win!")
 else:
     print("You lose. Ha!")
-word_guessed = (letter_guessed1 + letter_guessed2 + letter_guessed3 + letter_guessed4 + letter_guessed5 +
-                    letter_guessed6 + letter_guessed7 + letter_guessed8 + letter_guessed9 + letter_guessed10)
--- notes/Stuff To Remember.py
+++ notes/Stuff To Remember.py
@@ -1,1 +1,13 @@
+import random
 # Terminal = the display bar that pops up when you run a code
+random_word_list = ["pricy", "roast", "tenor", "tense", "loser", "train", "tears", "jerky", "abhor", "siren", "stair",
+                    "arise", "tires", "arson", "jacky", "brick", "chair", "tread", "trail", "pouch", "abdomens",
+                    "juxtaposed"]
+truly_random_word = random.choice(random_word_list)
+letters = list(truly_random_word)
+print(letters)
+letters2 = truly_random_word[0] + truly_random_word[1]
+print(letters2)
+if 0 == 0:
+    word = letters + list("     ")
+print(word)

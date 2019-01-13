65791ff6c09d86bc77e7389c5f6559ef8713a990
dzpu <42391788+dzpu@users.noreply.github.com>
2019-01-13 14:47:26 -0800

    Work at Home 1-13-19

--- notes/CJ Bergen Hangman Test 2.py
+++ notes/CJ Bergen Hangman Test 2.py
@@ -2,23 +2,25 @@ import random
 guesses = 10
 word = ["Crypt", "roast", "tenor", "tense", "loser", "train", "tears", "jerky", "abhor", "siren", "stair",  "arise",
         "tires", "arson", "jacky", "brick", "chair", "tread", "trail", "pouch", "abdomens",  "juxtaposed"]
-word_guessed = ["_____"]
+word_guessed = ["_____?"]
 truly_random_word = "Crypt"
 random.choice(word)
+"""
 if truly_random_word == "abdomens":
     word_guessed = ["________"]
 elif truly_random_word == "juxtaposed":
     word_guessed = ["_________"]
 elif truly_random_word == "Crypt":
     word_guessed = ["_____?"]
+"""
 print("".join(word_guessed))
 while guesses > 0 and not "".join(word_guessed) == truly_random_word:
     print("You have %s guesses left" % guesses)
-    letter_guessed = input("Guess a lowercase letter ")
+    letter_guessed = input("Guess a letter ")
     guesses -= 1
     for i in range(len(truly_random_word)):
         if truly_random_word[i] == letter_guessed:
-            word_guessed.insert(i, letter_guessed)
+            word_guessed[i] = letter_guessed
             print("".join(word_guessed))
             guesses += 1
 if "".join(word_guessed) == truly_random_word:

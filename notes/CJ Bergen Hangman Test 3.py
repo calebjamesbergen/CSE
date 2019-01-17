ea9c835ada3cb173fd01a91406343680e3201773
dzpu <42391788+dzpu@users.noreply.github.com>
2019-01-16 17:01:10 -0800

    Finishing Hangman

--- notes/CJ Bergen Hangman Test 3.py
+++ notes/CJ Bergen Hangman Test 3.py
@@ -3,10 +3,8 @@ guesses = 10
 did_you_win = False
 word = ["Crypt", "roast", "tenor", "tense", "loser", "train", "tears", "jerky", "abhor", "siren", "stair",  "arise",
         "tires", "arson", "jacky", "brick", "chair", "tread", "trail", "pouch", "abdomens",  "juxtaposed"]
-word_guessed = []
-truly_random_word = "Crypt?"
-# random.choice(word)
-random.choice(word)
+word_guessed = "_____"
+truly_random_word = random.choice(word)
 if truly_random_word == "abdomens":
     word_guessed = "________"
 elif truly_random_word == "juxtaposed":
@@ -27,9 +25,10 @@ while guesses > 0 and not word_guessed == truly_random_word and not did_you_win:
             did_you_win = True
     else:
         for i in range(len(truly_random_word)):
-            if truly_random_word[i] == letter_guessed:
+            if truly_random_word[i] == letter_guessed or truly_random_word[i] == str.lower(letter_guessed)\
+                    or truly_random_word[i] == str.upper(letter_guessed):
                 guesses += 1
-                word_guessed_letters[i] = letter_guessed
+                word_guessed_letters[i] = truly_random_word[i]
         word_guessed = "".join(word_guessed_letters)
         print(word_guessed)
 if word_guessed == truly_random_word:

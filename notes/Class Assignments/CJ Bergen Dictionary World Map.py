db71365b964f6721b62ca6f940ca4bf17f726a37
dzpu <42391788+dzpu@users.noreply.github.com>
2019-01-31 14:36:16 -0800

    Class Work 1-31-19

--- notes/Class Assignments/CJ Bergen Dictionary World Map.py
+++ notes/Class Assignments/CJ Bergen Dictionary World Map.py
@@ -14,3 +14,24 @@ world_map = {
         }
     }
 }
+
+# Other variables
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+current_node = world_map["R19A"]  # This is your current location
+playing = True
+
+# Controller
+while playing:
+    print(current_node["NAME"])
+
+    command = input(">")
+    if command.lower() in ["q", "quit", "exit"]:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+    else:
+        print("Command not recognized")

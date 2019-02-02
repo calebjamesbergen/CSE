ca9ccaf3f7b63f649bda4203508c9181d7276792
dzpu <42391788+dzpu@users.noreply.github.com>
2019-02-01 19:14:28 -0800

    Work at Home 2-1-19

--- notes/Zendikar 2.py
+++ notes/Zendikar 2.py
@@ -1,157 +1,158 @@
-raven_gorge_map = {
-    "Spawn Point Sheltered Valley": {
-        "NAME": "Spawn Point Sheltered Valley",
-        "DESCRIPTION": "Test room",
-        "PATHS": {
-            "NORTH": "Raven Gorge 1"
+world_map = {
+    "Raven Gorge Map": {
+        "Spawn Point Sheltered Valley": {
+            "NAME": "Spawn Point Sheltered Valley",
+            "DESCRIPTION": "Test room",
+            "PATHS": {
+                "NORTH": "Raven Gorge 1"
+            }
+        },
+        "Raven Gorge 1": {
+            "NAME": "Raven Gorge 1",
+            "DESCRIPTION": "Nothing",
+            "PATHS": {
+                "SOUTH": "Spawn Point",
+                "NORTH": "Raven Gorge 2"
+            }
+        },
+        "Raven Gorge 2": {
+            "NAME": "Raven Gorge 2",
+            "DESCRIPTION": "Eldrazi Scout",
+            "PATHS": {
+                "SOUTH": "Raven Gorge 1",
+                "NORTH": "Raven Gorge 3",
+                "WEST": "Raven Gorge 2 Left"
+            }
+        },
+        "Raven Gorge 2 Left": {
+          "NAME": "Raven Gorge 2 Left",
+          "DESCRIPTION": "25 Gold",
+          "PATHS": {
+                "EAST": "Raven Gorge 2"
+            }
+        },
+        "Raven Gorge 3": {
+            "NAME": "Raven Gorge 3",
+            "DESCRIPTION": "Eldrazi Scion",
+            "PATHS": {
+                "EAST": "Raven Gorge 3 Right",
+                "NORTH": "Raven Gorge 4",
+                "SOUTH": "Raven Gorge 2"
+            }
+        },
+        "Raven Gorge 3 Right": {
+            "NAME": "Raven Gorge 3 Right",
+            "DESCRIPTION": "Eldrazi Devastator",
+            "PATHS": {
+                "WEST": "Raven Gorge 3"
+            }
+        },
+        "Raven Gorge 4": {
+            "NAME": "Raven Gorge 4",
+            "DESCRIPTION": "Shop",
+            "PATHS": {
+                "Spawn Point Sheltered Valley"
+            }
         }
     },
-    "Raven Gorge 1": {
-        "NAME": "Raven Gorge 1",
-        "DESCRIPTION": "Nothing",
-        "PATHS": {
-            "SOUTH": "Spawn Point",
-            "NORTH": "Raven Gorge 2"
-        }
-    },
-    "Raven Gorge 2": {
-        "NAME": "Raven Gorge 2",
-        "DESCRIPTION": "Eldrazi Scout",
-        "PATHS": {
-            "SOUTH": "Raven Gorge 1",
-            "NORTH": "Raven Gorge 3",
-            "WEST": "Raven Gorge 2 Left"
-        }
-    },
-    "Raven Gorge 2 Left": {
-      "NAME": "Raven Gorge 2 Left",
-      "DESCRIPTION": "25 Gold",
-      "PATHS": {
-            "EAST": "Raven Gorge 2"
-        }
-    },
-    "Raven Gorge 3": {
-        "NAME": "Raven Gorge 3",
-        "DESCRIPTION": "Eldrazi Scion",
-        "PATHS": {
-            "EAST": "Raven Gorge 3 Right",
-            "NORTH": "Raven Gorge 4",
-            "SOUTH": "Raven Gorge 2"
-        }
-    },
-    "Raven Gorge 3 Right": {
-        "NAME": "Raven Gorge 3 Right",
-        "DESCRIPTION": "Eldrazi Devastator",
-        "PATHS": {
-            "WEST": "Raven Gorge 3"
-        }
-    },
-    "Raven Gorge 4": {
-        "NAME": "Raven Gorge 4",
-        "DESCRIPTION": "Shop",
-        "PATHS": {
-            "Spawn Point Sheltered Valley"
+
+    "Sheltered Valley Map": {
+        "Spawn Point Sheltered Valley": {
+            "NAME": "Spawn Point Sheltered Valley",
+            "DESCRIPTION": "Spawn Point",
+            "PATHS": {
+                "WEST": "Sheltered Valley 1",
+                "EAST": "Sheltered Valley 4"
+            }
+        },
+        "Sheltered Valley 1": {
+            "NAME": "Sheltered Valley 1",
+            "DESCRIPTION": "Spawn Point",
+            "PATHS": {
+                "SOUTH": "Sheltered Valley 7",
+                "NORTH": "Sheltered Valley 2"
+            }
+        },
+        "Sheltered Valley 2": {
+            "NAME": "Sheltered Valley 2",
+            "DESCRIPTION": "Dead body with 50 gold",
+            "PATHS": {
+                "SOUTH": "Sheltered Valley 1",
+                "NORTH": "Sheltered Valley 3"
+            }
+        },
+        "Sheltered Valley 3": {
+            "NAME": "Sheltered Valley 3",
+            "DESCRIPTION": "Stream",
+            "PATHS": {
+                "SOUTH": "Sheltered Valley 2",
+                "NORTH": "Mountain"
+            }
+        },
+        "Mountain": {
+            "NAME": "Mountain",
+            "DESCRIPTION": "Mountain",
+            "PATHS": {
+                "": "Spawn Point Mountain"
+            }
+        },
+        "Sheltered Valley 7": {
+            "NAME": "Sheltered Valley 7",
+            "DESCRIPTION": "Strange Noise",
+            "PATHS": {
+                "SOUTH": "Sheltered Valley 8"
+            }
+        },
+        "Sheltered Valley 8": {
+            "NAME": "Sheltered Valley 8",
+            "DESCRIPTION": "Making a fire",
+            "PATHS": {
+                "SOUTH": "Great Desert",
+                "NORTH": "Sheltered Valley 7"
+            }
+        },
+        "Desert": {
+            "NAME": "Desert",
+            "DESCRIPTION": "Desert",
+            "PATHS": {
+                "": "Spawn Point Desert"
+            }
+        },
+        "Sheltered Valley 4": {
+            "NAME": "Sheltered Valley 4",
+            "DESCRIPTION": "Mass of trees",
+            "PATHS": {
+                "NORTH": "Sheltered Valley 5",
+                "EAST": "Sheltered Valley 6"
+            }
+        },
+        "Sheltered Valley 5": {
+            "NAME": "Sheltered Valley 5",
+            "DESCRIPTION": "Heavy canopy",
+            "PATHS": {
+                "NORTH": "Lake",
+                "SOUTH": "Sheltered Valley 4"
+            }
+        },
+        "Lake": {
+            "NAME": "Lake",
+            "DESCRIPTION": "Lake",
+            "PATHS": {
+                "": "Spawn Point Lake"
+            }
+        },
+        "Sheltered Valley 6": {
+            "NAME": "Sheltered Valley 6",
+            "DESCRIPTION": "Cabin, instant death",
+            "PATHS": {}
+            # None
         }
     }
 }
-
-sheltered_valley_map = {
-    "Spawn Point Sheltered Valley": {
-        "NAME": "Spawn Point Sheltered Valley",
-        "DESCRIPTION": "Spawn Point",
-        "PATHS": {
-            "WEST": "Sheltered Valley 1",
-            "EAST": "Sheltered Valley 4"
-        }
-    },
-    "Sheltered Valley 1": {
-        "NAME": "Sheltered Valley 1",
-        "DESCRIPTION": "Spawn Point",
-        "PATHS": {
-            "SOUTH": "Sheltered Valley 7",
-            "NORTH": "Sheltered Valley 2"
-        }
-    },
-    "Sheltered Valley 2": {
-        "NAME": "Sheltered Valley 2",
-        "DESCRIPTION": "Dead body with 50 gold",
-        "PATHS": {
-            "SOUTH": "Sheltered Valley 1",
-            "NORTH": "Sheltered Valley 3"
-        }
-    },
-    "Sheltered Valley 3": {
-        "NAME": "Sheltered Valley 3",
-        "DESCRIPTION": "Stream",
-        "PATHS": {
-            "SOUTH": "Sheltered Valley 2",
-            "NORTH": "Mountain"
-        }
-    },
-    "Mountain": {
-        "NAME": "Mountain",
-        "DESCRIPTION": "Mountain",
-        "PATHS": {
-            "": "Spawn Point Mountain"
-        }
-    },
-    "Sheltered Valley 7": {
-        "NAME": "Sheltered Valley 7",
-        "DESCRIPTION": "Strange Noise",
-        "PATHS": {
-            "SOUTH": "Sheltered Valley 8"
-        }
-    },
-    "Sheltered Valley 8": {
-        "NAME": "Sheltered Valley 8",
-        "DESCRIPTION": "Making a fire",
-        "PATHS": {
-            "SOUTH": "Great Desert",
-            "NORTH": "Sheltered Valley 7"
-        }
-    },
-    "Desert": {
-        "NAME": "Desert",
-        "DESCRIPTION": "Desert",
-        "PATHS": {
-            "": "Spawn Point Desert"
-        }
-    },
-    "Sheltered Valley 4": {
-        "NAME": "Sheltered Valley 4",
-        "DESCRIPTION": "Mass of trees",
-        "PATHS": {
-            "NORTH": "Sheltered Valley 5",
-            "EAST": "Sheltered Valley 6"
-        }
-    },
-    "Sheltered Valley 5": {
-        "NAME": "Sheltered Valley 5",
-        "DESCRIPTION": "Heavy canopy",
-        "PATHS": {
-            "NORTH": "Lake",
-            "SOUTH": "Sheltered Valley 4"
-        }
-    },
-    "Lake": {
-        "NAME": "Lake",
-        "DESCRIPTION": "Lake",
-        "PATHS": {
-            "": "Spawn Point Lake"
-        }
-    },
-    "Sheltered Valley 6": {
-        "NAME": "Sheltered Valley 6",
-        "DESCRIPTION": "Cabin, instant death",
-        "PATHS": {}
-        # None
-    }
-}
-
 
 directions = ["NORTH", "SOUTH", "EAST", "WEST", ""]
 alive_raven_gorge = True
-current_node = raven_gorge_map["Spawn Point Sheltered Valley"]
+current_node = world_map["Raven Gorge Map"]["Spawn Point Sheltered Valley"]
 playing = True
 read_starting_text = False
 did_you_beat_raven_gorge = False
@@ -161,30 +162,49 @@ armor = "Leather armor"
 defence = 1
 total_gold = 0
 total_health = 5
+health_potions_owned = 0
+level = 0
+exp = 15
+exp_to_level_up = 20
 eldrazi_scout_attack = 1
 eldrazi_scout_health = 2
 eldrazi_scion_attack = 2
 eldrazi_scion_health = 3
 
+
+def check_self():
+    print("You have this weapon: %s" % weapon)
+    print("You deal %s damage" % damage)
+    print("You have this armor: %s" % armor)
+    print("You have %s defence" % defence)
+    print("You have %s health" % total_health)
+    print("You have %s gold" % total_gold)
+    print("You are level %s" % level)
+    print("You are %s exp away from leveling up" % (exp_to_level_up - exp))
+
+
 while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
     if not read_starting_text:
         print("Welcome to Zendikar")
         print("This is a game of skill and quite a bit of luck")
+        print("Try checking your stats and inventory by typing i")
         read_starting_text = True
         print("You are in %s" % current_node["NAME"])
     command = input(">")
     if command.lower() in ["q", "quit", "exit"]:
         playing = False
+    elif command.lower() == "i":
+        check_self()
     elif command in directions:
         try:
             room_name = current_node["PATHS"][command]
-            current_node = raven_gorge_map[room_name]
+            current_node = world_map["Raven Gorge Map"][room_name]
         except KeyError:
-            print("I can't go that way")
+            print("You can't go that way")
     else:
         print("Command not recognized")
     if current_node["NAME"] == "Raven Gorge 4":
-        current_node = sheltered_valley_map["Spawn Point Sheltered Valley"]
+        current_node = world_map["Sheltered Valley Map"]["Spawn Point Sheltered Valley"]
         did_you_beat_raven_gorge = True
     if current_node["NAME"] == "Raven Gorge 1":
         print("You are in %s" % current_node["NAME"])
@@ -204,6 +224,13 @@ while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
                 print("You did %s damage to the eldrazi scout" % damage)
                 eldrazi_scout_health -= damage
                 print("The eldrazi scion has %s health left" % eldrazi_scout_health)
+            if eldrazi_fight == "FLEE":
+                alive_raven_gorge = False
+                playing = False
+                print("You tried to flee but you tripped and the eldrazi killed you")
+                print("You died")
+                print("Try again")
+                break
         if eldrazi_scout_health <= 0:
             print("You slayed the eldrazi")
             print("You got 5 gold")

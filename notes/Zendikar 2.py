b5ba28ad75c1e44892e6a030485ac1f5dac73a98
dzpu <42391788+dzpu@users.noreply.github.com>
2019-02-06 14:35:34 -0800

    Class Work 2-6-19

--- notes/Zendikar 2.py
+++ notes/Zendikar 2.py
@@ -11,7 +11,7 @@ world_map = {
             "NAME": "Raven Gorge 1",
             "DESCRIPTION": "Nothing",
             "PATHS": {
-                "SOUTH": "Spawn Point",
+                "SOUTH": "Spawn Point Sheltered Valley",
                 "NORTH": "Raven Gorge 2"
             }
         },
@@ -51,7 +51,7 @@ world_map = {
             "NAME": "Raven Gorge 4",
             "DESCRIPTION": "Shop",
             "PATHS": {
-                "Spawn Point Sheltered Valley"
+                "SOUTH": "Raven Gorge 3"
             }
         }
     },
@@ -147,7 +147,91 @@ world_map = {
             "PATHS": {}
             # None
         }
-    }
+    },
+    "Mountain Map": {
+        "Spawn Point Mountain": {
+            "NAME": "Spawn Point Mountain",
+            "DESCRIPTION": "Spawn Point Mountain",
+            "PATHS": {
+                "WEST": "Mountain 1",
+                "EAST": "Mountain 6"
+            }
+        },
+        "Mountain 1": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+                "SOUTH": "Spawn Point Mountain",
+                "NORTH": "Mountain 2"
+            }
+        },
+        "Mountain 2": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+                "NORTH": "Mountain 5",
+                "SOUTH": "Mountain 3"
+            }
+        },
+        "Mountain 3": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+                "NORTH": "Mountain 2",
+                "SOUTH": "Mountain 4",
+                "EAST": "Mountain 5"
+            }
+        },
+        "Mountain 4": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+                "NORTH": "Mountain 3"
+            }
+        },
+        "Cave": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+                # None
+            }
+        },
+        "Mountain 5": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+                # None
+            }
+        },
+        "Mountain 6": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+
+            }
+        },
+        "Spawn Point Pit": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+
+            }
+        },
+        "Pit 1": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+
+            }
+        },
+        "Pit 2": {
+            "NAME": "",
+            "DESCRIPTION": "",
+            "PATHS": {
+
+            }
+        }
+    },
 }
 
 
@@ -159,7 +243,7 @@ def combine_health(health1, health_from_level1):
     return health1 + health_from_level1
 
 
-directions = ["NORTH", "SOUTH", "EAST", "WEST", "", "N", "S", "E", "W"]
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "", "N", "S", "E", "W", "Skip"]
 alive_raven_gorge = True
 current_node = world_map["Raven Gorge Map"]["Spawn Point Sheltered Valley"]
 playing = True
@@ -182,6 +266,9 @@ eldrazi_scout_health = 2
 eldrazi_scion_attack = 2
 eldrazi_scion_health = 3
 shop = "YES"
+did_you_beat_the_eldrazi_scout = False
+did_you_beat_the_eldrazi_scion = False
+in_shop = True
 
 
 def check_self():
@@ -216,13 +303,16 @@ while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
             print("You can't go that way")
     else:
         print("Command not recognized")
+    if command == "Skip":
+        current_node = world_map["Raven Gorge Map"]["Raven Gorge 4"]
+        total_gold += 100
     if current_node["NAME"] == "Raven Gorge 1":
         print("You are in %s" % current_node["NAME"])
         print("You start walking forward and you see an eldrazi scout north of you")
     elif current_node["NAME"] == "Raven Gorge 2":
         print("You are in %s" % current_node["NAME"])
         print("You see the eldrazi scout in front of you")
-        while eldrazi_scout_health > 0 and health > 0:
+        while eldrazi_scout_health > 0 and health > 0 and not did_you_beat_the_eldrazi_scout:
             print("The eldrazi scout attacked you")
             print("You took %i damage" % int(eldrazi_scout_attack - defence))
             health = health - (eldrazi_scout_attack - defence)
@@ -255,6 +345,7 @@ while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
                 health_from_level += 1
                 print("You leveled up")
                 print("Your damage and health increased by 1")
+        did_you_beat_the_eldrazi_scout = True
     elif current_node["NAME"] == "Raven Gorge 2 Left":
         print("You are in %s" % current_node["NAME"])
         print("You found 25 gold")
@@ -266,7 +357,8 @@ while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
         print("What would you like to do")
         eldrazi_scion_fight = input("ATTACK or FLEE")
         if eldrazi_scion_fight == "ATTACK":
-            while eldrazi_scion_health > 0 and combine_health(health, health_from_level) > 0:
+            while eldrazi_scion_health > 0 and combine_health(health, health_from_level) > 0 \
+                    and not did_you_beat_the_eldrazi_scion:
                 print("The eldrazi attacked you")
                 print("You took % i damage" % int(eldrazi_scion_attack - defence))
                 health -= int(eldrazi_scion_attack - defence)
@@ -277,17 +369,19 @@ while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
                 print("The eldrazi takes %s damage" % damage)
                 eldrazi_scion_health -= damage
                 print("The eldrazi has %s health left" % eldrazi_scion_health)
-            print("You defeated the eldrazi")
-            total_gold += 5
-            print("Now you have %s gold" % total_gold)
-            exp += 5
-            if exp >= exp_to_level_up:
-                exp -= 20
-                level += 1
-                damage_from_level += 1
-                health_from_level += 1
-                print("You leveled up")
-                print("Your damage and health increased by 1")
+            if eldrazi_scion_health <= 0:
+                print("You defeated the eldrazi")
+                total_gold += 5
+                print("Now you have %s gold" % total_gold)
+                exp += 5
+                if exp >= exp_to_level_up:
+                    exp -= 20
+                    level += 1
+                    damage_from_level += 1
+                    health_from_level += 1
+                    print("You leveled up")
+                    print("Your damage and health increased by 1")
+                    did_you_beat_the_eldrazi_scion = True
         if eldrazi_scion_fight == "FLEE":
             print("You tripped and the eldrazi killed you")
             print("You died")
@@ -305,35 +399,41 @@ while alive_raven_gorge and playing and not did_you_beat_raven_gorge:
     elif current_node["NAME"] == "Raven Gorge 4":
         print("You have reached the end of this area")
         print("Poof a shop appears")
-        print("This is what is offered")
-        print("Wood Sword: 5 Gold, Copper Armor: 5 Gold, Health Potion: 5 Gold")
-        print("What would you like to buy")
-        while total_gold > 0 and shop == "YES":
-            raven_gorge_shop = input("WOOD SWORD or COPPER ARMOR or HEALTH POTION")
-            if raven_gorge_shop == "WOOD SWORD" and total_gold >= 5:
-                total_gold -= 5
-                weapon = "WOOD SWORD"
-                damage = 3
-                print("You bought the Wood Sword")
-                print("Now you deal %s damage" % combine_attack(damage, damage_from_level))
-                print("Now you have %s gold" % total_gold)
-            if raven_gorge_shop == "COPPER ARMOR" and total_gold >= 5:
-                total_gold -= 5
-                armor = "COPPER ARMOR"
-                defence = 3
-                print("You bought the Copper Armor")
-                print("Now you have %s defence" % defence)
-                print("Now you have %s gold" % total_gold)
-            if raven_gorge_shop == "HEALTH POTION" and total_gold >= 5:
-                total_gold -= 5
-                health += 3
-                print("You bought the Health Potion")
-                print("Now you have %s health" % combine_health(health, health_from_level))
-                print("Now you have %s gold" % total_gold)
-            current_node = world_map["Sheltered Valley Map"]["Spawn Point Sheltered Valley"]
-            did_you_beat_raven_gorge = True
-            print("Would you like to remain in the shop")
-            shop = input("YES or NO")
+        print("Would you like to enter the shop")
+        choice = input("YES or NO")
+        if choice == "YES":
+            print("This is what is offered")
+            print("Wood Sword: 5 Gold, Copper Armor: 5 Gold, Health Potion: 5 Gold")
+            print("What would you like to buy")
+            while total_gold > 0 and shop == "YES" and in_shop:
+                raven_gorge_shop = input("WOOD SWORD or COPPER ARMOR or HEALTH POTION")
+                if raven_gorge_shop == "WOOD SWORD" and total_gold >= 5:
+                    total_gold -= 5
+                    weapon = "WOOD SWORD"
+                    damage = 3
+                    print("You bought the Wood Sword")
+                    print("Now you deal %s damage" % combine_attack(damage, damage_from_level))
+                    print("Now you have %s gold" % total_gold)
+                if raven_gorge_shop == "COPPER ARMOR" and total_gold >= 5:
+                    total_gold -= 5
+                    armor = "COPPER ARMOR"
+                    defence = 3
+                    print("You bought the Copper Armor")
+                    print("Now you have %s defence" % defence)
+                    print("Now you have %s gold" % total_gold)
+                if raven_gorge_shop == "HEALTH POTION" and total_gold >= 5:
+                    total_gold -= 5
+                    health += 3
+                    print("You bought the Health Potion")
+                    print("Now you have %s health" % combine_health(health, health_from_level))
+                    print("Now you have %s gold" % total_gold)
+                print("Would you like to remain in the shop")
+                shop = input("YES or NO")
+                if shop == "NO":
+                    current_node = world_map["Sheltered Valley Map"]["Spawn Point Sheltered Valley"]
+                    did_you_beat_raven_gorge = True
+        if choice == "NO":
+            pass
 
 alive_sheltered_valley = True
 did_you_beat_sheltered_valley = False

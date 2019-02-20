fd90fbae022739f9aa329a24192e3e6a891a7a05
dzpu <42391788+dzpu@users.noreply.github.com>
2019-02-19 20:03:23 -0800

    Work at Home 2-19-19

--- notes/Zendikar 3.py
+++ notes/Zendikar 3.py
@@ -12,7 +12,7 @@ class Monster:
 
 
 class Person(object):
-    def __init__(self, assigned_room=None):
+    def __init__(self, assigned_room="Spawn Point Raven Gorge"):
         self.alive_raven_gorge = True
         self.directions = ["north", "south", "east", "west", "", "Skip"]
         self.current_node = assigned_room
@@ -21,12 +21,12 @@ class Person(object):
         self.did_you_beat_raven_gorge = False
         self.weapon = "Stick"
         self.damage = 1
-        self.damage_from_level = 0
+        self.damage_from_level = 1
         self.armor = "Leather armor"
         self.defence = 1
         self.total_gold = 0
         self.health = 5
-        self.health_from_level = 0
+        self.health_from_level = 1
         self.level = 1
         self.exp = 15
         self.exp_to_level_up = 20
@@ -44,6 +44,7 @@ class Person(object):
         self.shop3 = "YES"
         self.total_health = self.health + self.health_from_level
         self.total_damage = self.damage + self.damage_from_level
+        self.defence = 1
 
     def combine_health(self):
         self.total_health = self.health + self.health_from_level
@@ -58,6 +59,9 @@ class Person(object):
 
     def get_health(self):
         return self.health + self.health_from_level
+
+    def damage_you_take(self, monster_attack):
+        return monster_attack - self.defence
 
     def level_up(self):
         if self.exp >= self.exp_to_level_up:
@@ -110,26 +114,26 @@ class Person(object):
             while health > 0 and not did_you_beat_it:
                 print("You swung your %s at the %s" % (self.weapon, name))
                 print("The %s took %s damage" % (name, self.get_attack()))
-                health -= attack
+                health -= you.get_attack()
                 print("Now the %s has %s health left" % (name, health))
-                print("The %s attacked you and you took %s damage" % (name, attack))
-                self.health -= attack
+                print("The %s attacked you and you took %s damage" % (name, you.damage_you_take(attack)))
+                self.health -= you.damage_you_take(attack)
                 print("Now you have %s health" % self.get_health())
-            if health <= 0:
-                if you.total_health + you.health_from_level > 0:
-                    print("You defeated the %s" % name)
-                    print("You got %s gold" % gold)
-                    self.total_gold += gold
-                    print("Now you have %s gold" % gold)
-                    self.exp += exp
-                    print("You got %s exp" % exp)
-                    print("Now you have %s exp" % self.exp)
-                    self.level_up()
-                    eldrazi_scout.did_you_beat_monster = True
-            else:
+            if you.get_health() > 0:
+                print("You defeated the %s" % name)
+                print("You got %s gold" % gold)
+                self.total_gold += gold
+                print("Now you have %s gold" % gold)
+                self.exp += exp
+                print("You got %s exp" % exp)
+                print("Now you have %s exp" % self.exp)
+                self.level_up()
+                eldrazi_scout.did_you_beat_monster = True
+            elif you.get_health() <= 0:
                 print("You died")
                 print("Try again")
                 self.playing = False
+                afgaafdsf
         if fighting == "FLEE" or "flee":
             print("You died")
             print("Try again")
@@ -137,8 +141,8 @@ class Person(object):
 
 
 class Room(object):
-    def __init__(self, name, north, south, east, west, surprise, room_text,
-                 surprise_name="", instant_death=False, monster_in_it=[], gold_in_it=False, how_much_gold=0):
+    def __init__(self, name, north, south, east, west, surprise, room_text, surprise_name="", instant_death=False,
+                 monster_in_it=False, monster_name="", gold_in_it=False, how_much_gold=0):
         self.monsters = monster_in_it
         self.north = north
         self.name = name
@@ -151,11 +155,12 @@ class Room(object):
         self.instant_death = instant_death
         self.gold_in_it = gold_in_it
         self.how_much_gold = how_much_gold
+        self.monster_name = monster_name
 
-    def run_room(self, name, health,
-                 attack, gold, exp, did_you_beat_it):
+    def run_room(self, name=None, health=None, attack=None, gold=None, exp=None, did_you_beat_it=None):
         print(self.name)
-        print(self.room_text)
+        if self.room_text:
+            print(self.room_text)
         if self.gold_in_it:
             print("You found %s gold" % self.how_much_gold)
             you.total_gold += self.how_much_gold
@@ -163,15 +168,15 @@ class Room(object):
         if self.monsters:
             print("A/an %s appeared" % name)
             you.fight(name, health, attack, gold, exp, did_you_beat_it)
-        you.command()
         if self.instant_death:
             you.playing = False
             print("You died")
             print("Try again")
+        if you.playing:
+            you.command()
 
 
 you = Person()
-
 
 # Monsters
 eldrazi_scout = Monster("Eldrazi Scout", 2, 1, 5, 5, False)
@@ -186,18 +191,37 @@ volcano_hellion = Monster("Volcano Hellion", 15, 10, 15, 10, False)
 # Rooms
 spawn_point_raven_gorge = Room("Spawn Point Raven Gorge", "raven_gorge1", None,
                                None, None, None, None)
-raven_gorge1 = Room("Raven Gorge 2", "raven_gorge2", None, None, "raven_gorge2_left", None,
+raven_gorge1 = Room("Raven Gorge 1", "raven_gorge2", None, None, "raven_gorge2_left", None,
                     "There is an eldrazi scout north of you")
 raven_gorge2 = Room("Raven Gorge 2", "raven_gorge3", "raven_gorge1", None, "raven_gorge2_left", None,
-                    "An eldrazi scout is here")
-raven_gorge2_left = Room("Raven Gorge 2 Left", None, None, "raven_gorge2", None, None, "", "", False, [], True, 25)
-raven_gorge3 = Room("Raven Gorge 3", "raven_gorge4", "raven_gorge2", "raven_gorge3_right", None, None, None)
+                    "An eldrazi scout is here", "", False, True, "Eldrazi Scout")
+raven_gorge2_left = Room("Raven Gorge 2 Left", None, None, "raven_gorge2", None, None, "", "", False, False, "", True,
+                         25)
+raven_gorge3 = Room("Raven Gorge 3", "raven_gorge4", "raven_gorge2", "raven_gorge3_right", None, None, None, "", False,
+                    True, "Eldrazi Scion")
 raven_gorge3_right = Room("Raven Gorge 3 Right", None, None, None, "raven_gorge3", True,
                           "An eldrazi devastator appeared and ripped you apart", "Eldrazi Devastator", True)
 raven_gorge4 = Room("Raven Gorge 4", None, "raven_gorge3", None, None, None, None)
 
-
 you.current_node = spawn_point_raven_gorge
+
+while you.playing:
+    if you.current_node.name == "Spawn Point Raven Gorge":
+        spawn_point_raven_gorge.run_room()
+    if you.current_node.name == "Raven Gorge 1":
+        raven_gorge1.run_room()
+    if you.current_node.name == "Raven Gorge 2":
+        raven_gorge2.run_room("Eldrazi Scout", eldrazi_scout.monster_health, eldrazi_scout.monster_attack,
+                              eldrazi_scout.monster_gold, eldrazi_scout.monster_exp, eldrazi_scout.did_you_beat_monster)
+    if you.current_node.name == "Raven Gorge 2 Left":
+        raven_gorge2_left.run_room()
+    if you.current_node.name == "Raven Gorge 3":
+        raven_gorge3.run_room("Eldrazi Scion", eldrazi_scion.monster_health, eldrazi_scion.monster_attack,
+                              eldrazi_scion.monster_gold, eldrazi_scion.monster_exp, eldrazi_scion.did_you_beat_monster)
+    if you.current_node.name == "Raven Gorge 3 Right":
+        raven_gorge3_right.run_room()
+    if you.current_node.name == "Raven Gorge 4":
+        raven_gorge4.run_room()
 
 
 while you.alive_raven_gorge and you.playing and not you.did_you_beat_raven_gorge:
@@ -292,7 +316,7 @@ while you.alive_raven_gorge and you.playing and not you.did_you_beat_raven_gorge
                 if raven_gorge_shop == "COPPER ARMOR" and you.total_gold >= 5:
                     you.total_gold -= 5
                     armor = "COPPER ARMOR"
-                    defence = 3
+                    you.defence = 3
                     print("You bought the Copper Armor")
                     print("Now you have %s defence" % defence)
                     print("Now you have %s gold" % you.total_gold)

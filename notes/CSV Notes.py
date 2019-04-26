ad1f97bb5ba88b595d5ef4500a49649168c26f4d
dzpu <42391788+dzpu@users.noreply.github.com>
2019-04-26 14:38:20 -0700

    Class Work 4-26-19

--- notes/CSV Notes.py
+++ notes/CSV Notes.py
@@ -1,5 +1,24 @@
 import csv
-a = 0
+
+
+def validate(num: str):
+    if is_it_odd(num) and is_it_even(num):
+        return True
+    return False
+
+
+def is_it_odd(num: str):
+    first_number = int(num[0])
+    if first_number % 2 != 0:
+        return True
+    return False
+
+
+def is_it_even(num: str):
+    first_number = int(num[1])
+    if first_number % 2 == 0:
+        return True
+    return False
 
 # with open("Book1.csv", "r") as old_csv:
 # #     reader = csv.reader(old_csv)
@@ -18,7 +37,22 @@ a = 0
 # #         old_number = row[0]
 # #         print(int(old_number) + 1)
 
-# R means read only
+# 'r' means read only
+
+# with open("Book1.csv", "r") as old_csv:
+#     with open("MyNewFile.csv", "w", newline='') as new_csv:
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         print("Processing...")
+#
+#         for row in reader:
+#             old_number = row[0]
+#             first_num = int(old_number[0])
+#             if old_number[14] and not old_number[15]:
+#                 writer.writerow(row)
+#
+#     print("Done")
+
 
 with open("Book1.csv", "r") as old_csv:
     with open("MyNewFile.csv", "w", newline='') as new_csv:
@@ -29,7 +63,7 @@ with open("Book1.csv", "r") as old_csv:
         for row in reader:
             old_number = row[0]
             first_num = int(old_number[0])
-            if first_num == 4:
+            if validate(old_number):
                 writer.writerow(row)
 
     print("Done")
